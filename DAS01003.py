b8e5ca2091a3b50fdb047d7fa23da2c76f0a009f
dinhgihuong <huongdtt.b17at089@gmail.com>
2021-03-03 15:27:46 +0700

    update DAS01003

--- DAS01003.py
+++ DAS01003.py
@@ -11,23 +11,24 @@ def dao(a,b) :
 def hv_ke_tiep(a):
     n = len(a) - 1
     j =  n-1
+    # vị trí có chỉ số j đầu tiên thỏa mãn aj < aj+1
     while j >= 0 and a[j] >= a[j+1]:
         j -= 1
     if j >= 0 :
         k = n
+        # ak là số nhỏ nhất còn lớn hơn aj
         while a[k] < a[j]:
             k -= 1
+        # Đổi chỗ aj cho ak
         a[j] , a[k] = dao(a[j], a[k])
         r = j + 1
         s = n
+        # Lật ngược đoạn aj+1, …, an.
         while r <= s:
             a[r], a[s] = dao(a[r], a[s])
             r += 1
             s -= 1
-    # kq = ""
-    # for i in a:
-    #     kq = kq + str(i) + " "
-    # print(kq)
+
 
 
 
